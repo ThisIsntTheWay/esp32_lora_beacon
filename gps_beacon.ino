6584479f0b29aa0f2b7aa83f5928480fa6795bfb
The man <ThisIsntTheWay@users.noreply.github.com>
2020-04-28 19:30:09 +0200

    Bugfixes

--- gps_beacon.ino
+++ gps_beacon.ino
@@ -24,7 +24,7 @@ unsigned int counter = 0;
 // Init libraries
 TTN_esp32 ttn;
 TTN_CayenneLPP lpp;
-TinyGPSplus gps;
+TinyGPSPlus gps;
 HardwareSerial sGPS(1);
 
 // TTN "Join" message
@@ -42,7 +42,7 @@ void message(const uint8_t* payload, size_t size, int rssi)
 void setup()
 {
     // Start listening to HW Serial channel
-    sGPS.begin(GPSBaud, SERIAL_8IN1, RXPin, TXPin);
+    sGPS.begin(GPSBaud, SERIAL_8N1, RXPin, TXPin);
     
     // Init Heltec OLED
     Heltec.begin(true /*DisplayEnable Enable*/, true /*Heltec.Heltec.Heltec.LoRa Disable*/, true /*Serial Enable*/, true /*PABOOST Enable*/, BAND /*long BAND*/);
@@ -98,7 +98,7 @@ void loop()
     // Send out lpp packet to TTN
     if (ttn.sendBytes(lpp.getBuffer(), lpp.getSize())) {
         // Write sent lpp packet to serial.
-        Serial.printf("Temp: %f TTN_CayenneLPP: %d %x %02X%02X\n", nb, lpp.getBuffer()[0], lpp.getBuffer()[1],
+        Serial.printf("TTN_CayenneLPP: %f %d %02X%02X\n", lpp.getBuffer()[0], lpp.getBuffer()[1],
             lpp.getBuffer()[2], lpp.getBuffer()[3]);
     }
 
