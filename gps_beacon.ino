b88c202cd84806e3bb9ad11e187688eaeedbf881
The man <ThisIsntTheWay@users.noreply.github.com>
2020-04-28 20:29:43 +0200

    Implement v1.0

--- gps_beacon.ino
+++ gps_beacon.ino
@@ -6,7 +6,7 @@
 #include <HardwareSerial.h>
 
 // Set LoRa Band (868MHz, Europe)
-#define BAND    868E6
+#define BAND 868E6
 
 // TTN stuff
 // NOTE: The values requested are only applicable to a TTN-join via OTA!
@@ -16,8 +16,9 @@ const char* appKey = "- - -"; //TTN Application Key
 
 // HW Serial and GPS config
 // NOTE: Here, it's set to UART2.
-static const int RXPin = 16, TXPin = 17;
-static const uint32_t GPSBaud = 9600;
+#define RXD2 23
+#define TXD2 17
+#define GPSBaud 9600
 
 unsigned int counter = 0;
 
@@ -25,7 +26,6 @@ unsigned int counter = 0;
 TTN_esp32 ttn;
 TTN_CayenneLPP lpp;
 TinyGPSPlus gps;
-HardwareSerial sGPS(1);
 
 // TTN "Join" message
 void message(const uint8_t* payload, size_t size, int rssi)
@@ -42,7 +42,7 @@ void message(const uint8_t* payload, size_t size, int rssi)
 void setup()
 {
     // Start listening to HW Serial channel
-    sGPS.begin(GPSBaud, SERIAL_8N1, RXPin, TXPin);
+    Serial2.begin(GPSBaud, SERIAL_8N1, RXD2, TXD2);
     
     // Init Heltec OLED
     Heltec.begin(true /*DisplayEnable Enable*/, true /*Heltec.Heltec.Heltec.LoRa Disable*/, true /*Serial Enable*/, true /*PABOOST Enable*/, BAND /*long BAND*/);
@@ -51,7 +51,7 @@ void setup()
     Heltec.display->setFont(ArialMT_Plain_10);
     Heltec.display->clear();
     
-    Heltec.display->drawString(0, 0, "TTN Beacon - v0.5");
+    Heltec.display->drawString(0, 0, "TTN Beacon - v1.0");
     Heltec.display->drawString(0, 10, "Initialising...");
     Heltec.display->display();
   
@@ -85,22 +85,35 @@ void setup()
 }
 
 void loop()
-{
+{        
     // Encode/parse NMEA messages if the HW serial is available.
-    while (sGPS.available() > 0) {
-        gps.encode(sGPS.read());
+    while (Serial2.available() > 0) {
+        gps.encode(Serial2.read());
     }
+
+    float gLat = gps.location.lat();
+    float gLng = gps.location.lng();
+    float gAlt = gps.altitude.meters();
     
     // Construct lpp buffer.
     lpp.reset();
-    lpp.addGPS(1, gps.location.lat(), gps.location.lng(), gps.altitude.meters());
+    lpp.addGPS(1, gLat, gLng, gAlt);
 
     // Send out lpp packet to TTN
     if (ttn.sendBytes(lpp.getBuffer(), lpp.getSize())) {
         // Write sent lpp packet to serial.
-        Serial.printf("TTN_CayenneLPP: %f %d %02X%02X\n", lpp.getBuffer()[0], lpp.getBuffer()[1],
+        Serial.printf("CLPP packet: %f %d %02X%02X\n", lpp.getBuffer()[0], lpp.getBuffer()[1],
             lpp.getBuffer()[2], lpp.getBuffer()[3]);
     }
+
+    // Spit out info to serial
+    Serial.println();
+    Serial.print("LAT - "); Serial.println(gps.location.lat());
+    Serial.print("LON - "); Serial.println(gps.location.lng());
+    Serial.print("ALT - "); Serial.println(gps.altitude.meters());
+    Serial.print("SAT - "); Serial.println(gps.satellites.value());
+    Serial.print("HDOP - "); Serial.println(gps.hdop.value());
+    Serial.println("-----------------------");
 
     // Write to OLED
     Heltec.display->clear();
@@ -109,11 +122,12 @@ void loop()
     
     Heltec.display->drawString(0, 0, "Packets sent: ");
     Heltec.display->drawString(90, 0, String(counter));    
-    Heltec.display->drawString(0, 10, "GPS Satellites: ");
-    Heltec.display->drawString(90, 10, String(gps.satellites.value()));    
-    Heltec.display->drawString(0, 20, "Perceived HDOP: ");
-    Heltec.display->drawString(90, 20, String(gps.hdop.value()));
+    Heltec.display->drawString(0, 20, "GPS Satellites: ");
+    Heltec.display->drawString(90, 20, String(gps.satellites.value()));    
+    Heltec.display->drawString(0, 30, "Perceived HDOP: ");
+    Heltec.display->drawString(90, 30, String(gps.hdop.value()));
     Heltec.display->display();
+    
     counter++;
     delay(3000);
 }
